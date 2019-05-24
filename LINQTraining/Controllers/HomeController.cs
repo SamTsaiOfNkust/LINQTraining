e2b211bc14d3e996bd9b3db05732295aa90f9fa6
Sam Tsai <samtsai@nkust.edu.tw>
2019-05-24 16:50:56 +0800

    LINQ 初次使用

--- LINQTraining/Controllers/HomeController.cs
+++ LINQTraining/Controllers/HomeController.cs
@@ -8,9 +8,58 @@ namespace LINQTraining.Controllers
 {
     public class HomeController : Controller
     {
+        int[] Sources = new int[] { 49, 10, -10, 90, 80, 99, -20 };
         public ActionResult Index()
         {
+            #region 傳統寫法
+            /*
+            var Result = new List<int>();
+            foreach (var Row in Sources)
+            {
+                if (Row > 0)
+                    Result.Add(Row);
+            }
+            */
+            #endregion
+            // 使用LINQ
+            var Result = Sources.Where(s => s > 0).ToList();
+
+
+            ViewBag.SOURCE = Sources;
+            ViewBag.RESULT = Result;
             return View();
+        }
+        public ActionResult IndexWithSort()
+        {
+            #region 傳統寫法
+            /*
+            var Result = new List<int>();
+            foreach (var Row in Sources)
+            {
+                if (Row > 0)
+                    Result.Add(Row);
+            }
+            Comparison<int> comparison = delegate (int a, int b)
+            {
+                return b - a;
+            };
+            Result.Sort(comparison);
+            */
+            #endregion
+            // 使用LINQ
+            
+            var Result = Sources.Where(s => s > 0).OrderBy(s => s).ToList();
+            // or 
+            /*
+            var Result = 
+                from s in Sources
+                     where s > 0
+                     orderby s
+                     select s;
+                     */
+            ViewBag.SOURCE = Sources;
+            ViewBag.RESULT = Result;
+            return View("Index");
         }
 
         public ActionResult About()
